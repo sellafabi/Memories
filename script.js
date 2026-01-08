f2b6b9e79699093d062daaa3da65f52a648beddd
sellafabi <rosellafabillar13@gmail.com>
2026-01-09 00:15:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,43 +6,43 @@ const galleries = {
   jollibee: {
     title: "Jollibee",
     images: [
-      "images/assets/Jollibee L.jpg"
-      "images/assets/Jollibee P (2).jpg"
-      "images/assets/Jollibee P (3).jpg"
+      "images/assets/Jollibee L.jpg",
+      "images/assets/Jollibee P (2).jpg",
+      "images/assets/Jollibee P (3).jpg",
       "images/assets/Jollibee P.jpg"
     ]
   },
   mcdo: {
     title: "McDo",
     images: [
-      "images/assets/McDo L (3).jpg"
-      "images/assets/McDo L (4).jpg"
-      "images/assets/McDo L (5).jpg"
-      "images/assets/McDo L (6).jpg"
-      "images/assets/McDo L.jpg"
-      "images/assets/McDo P (2).jpg"
-      "images/assets/McDo P (3).jpg"
-      "images/assets/McDo P (4).jpg"
+      "images/assets/McDo L (3).jpg",
+      "images/assets/McDo L (4).jpg",
+      "images/assets/McDo L (5).jpg",
+      "images/assets/McDo L (6).jpg",
+      "images/assets/McDo L.jpg",
+      "images/assets/McDo P (2).jpg",
+      "images/assets/McDo P (3).jpg",
+      "images/assets/McDo P (4).jpg",
       "images/assets/McDo P.jpg"
     ]
   },
   christmas: {
     title: "Christmas Party 2025",
     images: [
-      "images/assets/ChirstmasParty2025 DazenG L (2).jpg"
-      "images/assets/ChirstmasParty2025 DazenG L (3).jpg"
-      "images/assets/ChirstmasParty2025 DazenG L (4).jpg"
-      "images/assets/ChirstmasParty2025 DazenG L (5).jpg"
-      "images/assets/ChirstmasParty2025 DazenG L.jpg"
-      "images/assets/ChirstmasParty2025 PG L (2).jpg"
-      "images/assets/ChirstmasParty2025 PG L.jpg"
-      "images/assets/ChirstmasParty2025 DazenG P (2).jpg"
-      "images/assets/ChirstmasParty2025 DazenG P (3).jpg"
-      "images/assets/ChirstmasParty2025 DazenG P (4).jpg"
-      "images/assets/ChirstmasParty2025 DazenG P (5).jpg"
-      "images/assets/ChirstmasParty2025 DazenG P (6).jpg"
-      "images/assets/ChirstmasParty2025 DazenG P (7).jpg"
-      "images/assets/ChirstmasParty2025 DazenG P (8).jpg"
+      "images/assets/ChirstmasParty2025 DazenG L (2).jpg",
+      "images/assets/ChirstmasParty2025 DazenG L (3).jpg",
+      "images/assets/ChirstmasParty2025 DazenG L (4).jpg",
+      "images/assets/ChirstmasParty2025 DazenG L (5).jpg",
+      "images/assets/ChirstmasParty2025 DazenG L.jpg",
+      "images/assets/ChirstmasParty2025 PG L (2).jpg",
+      "images/assets/ChirstmasParty2025 PG L.jpg",
+      "images/assets/ChirstmasParty2025 DazenG P (2).jpg",
+      "images/assets/ChirstmasParty2025 DazenG P (3).jpg",
+      "images/assets/ChirstmasParty2025 DazenG P (4).jpg",
+      "images/assets/ChirstmasParty2025 DazenG P (5).jpg",
+      "images/assets/ChirstmasParty2025 DazenG P (6).jpg",
+      "images/assets/ChirstmasParty2025 DazenG P (7).jpg",
+      "images/assets/ChirstmasParty2025 DazenG P (8).jpg",
       "images/assets/ChirstmasParty2025 DazenG P.jpg"
     ]
   }
